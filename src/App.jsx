06847d6527263e64b0b1cd59b237a481c78834a7
MJMahedi <mdrokibulhasan5321@gmail.com>
2025-05-21 21:04:29 +0600

    picture added for them

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import omivai from "./assets/omivai.jpg"
+import saraApu from "./assets/saraapu.jpg"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='flex items-center'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +17,26 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-xl'>Omi Vai + Sara Apu</h1>
+      <h1 className='text-4xl'>Omi Vai + Sara Apu</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4 grid grid-cols-2">
+        <div className="carousel-item">
+          <img
+            src={omivai}
+            className="rounded-box w-[full] h-[250px]" />
+        </div>
+        <div className="carousel-item">
+          <img
+            src={saraApu}
+            className="rounded-box h-[250px]" />
+        </div>
+        
+      </div>
     </>
   )
 }
